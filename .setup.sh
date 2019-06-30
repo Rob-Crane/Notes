1e473ec54401c07f3d1aa5be8cd9278f50ca9293
R Crane <28930454+Rob-Crane@users.noreply.github.com>
2019-06-29 23:10:35 -0700

    Add setup script.

---
+++ .setup.sh
@@ -0,0 +1,4 @@
+sudo apt-get install vim git
+git config --global user.email "28930454+Rob-Crane@users.noreply.github.com"
+git config --global user.name "R Crane"
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
